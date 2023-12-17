a67f262cb8c66d82c3f9a2fcc5f70c17408dde67
zcg <729505530@qq.com>
2023-12-17 15:28:45 +0800

    first edition

---
+++ custom_prompt.py
@@ -0,0 +1,279 @@
+import copy
+import os
+import re
+from typing import Dict
+
+import json
+from config_utils import DEFAULT_BUILDER_CONFIG_FILE, get_user_cfg_file
+from modelscope_agent.prompt.prompt import (KNOWLEDGE_PROMPT, PromptGenerator,
+                                            build_raw_prompt)
+
+from modelscope.utils.config import Config
+
+DEFAULT_SYSTEM_TEMPLATE = """
+
+# 工具
+
+## 你拥有如下工具：
+
+<tool_list>
+
+## 当你需要调用工具时，请在你的回复中穿插如下的工具调用命令，可以根据需求调用零次或多次：
+
+工具调用
+Action: 工具的名称，必须是<tool_name_list>之一
+Action Input: 工具的输入
+Observation: <result>工具返回的结果</result>
+Answer: 根据Observation总结本次工具调用返回的结果，如果结果中出现url，请不要展示出。
+
+```
+[链接](url)
+```
+
+# 指令
+"""
+
+DEFAULT_SYSTEM_TEMPLATE_WITHOUT_TOOL = """
+
+# 指令
+"""
+
+DEFAULT_INSTRUCTION_TEMPLATE = ''
+
+DEFAULT_USER_TEMPLATE = """(你正在扮演<role_name>，你可以使用工具：<tool_name_list><knowledge_note>)<file_names><user_input>"""
+
+DEFAULT_USER_TEMPLATE_WITHOUT_TOOL = """(你正在扮演<role_name><knowledge_note>) <file_names><user_input>"""
+
+DEFAULT_EXEC_TEMPLATE = """Observation: <result><exec_result></result>\nAnswer:"""
+
+TOOL_DESC = (
+    '{name_for_model}: {name_for_human} API。 {description_for_model} 输入参数: {parameters}'
+)
+
+
+class CustomPromptGenerator(PromptGenerator):
+
+    def __init__(self,
+                 system_template=DEFAULT_SYSTEM_TEMPLATE,
+                 instruction_template=DEFAULT_INSTRUCTION_TEMPLATE,
+                 user_template=DEFAULT_USER_TEMPLATE,
+                 exec_template=DEFAULT_EXEC_TEMPLATE,
+                 assistant_template='',
+                 sep='\n\n',
+                 prompt_max_length=1000,
+                 **kwargs):
+        super().__init__(system_template, instruction_template, user_template,
+                         exec_template, assistant_template, sep,
+                         prompt_max_length)
+        # hack here for special prompt, such as add an addition round before user input
+        self.add_addition_round = kwargs.get('add_addition_round', False)
+        self.addition_assistant_reply = kwargs.get('addition_assistant_reply',
+                                                   '')
+        builder_cfg_file = get_user_cfg_file(
+            uuid_str=kwargs.get('uuid_str', ''))
+        builder_cfg = Config.from_file(builder_cfg_file)
+        self.builder_cfg = builder_cfg
+        self.knowledge_file_name = kwargs.get('knowledge_file_name', '')
+
+    def _update_user_prompt_without_knowledge(self, task, tool_list, **kwargs):
+        if len(tool_list) > 0:
+            # user input
+            user_input = self.user_template.replace('<role_name>',
+                                                    self.builder_cfg.name)
+            user_input = user_input.replace(
+                '<tool_name_list>',
+                ','.join([tool.name for tool in tool_list]))
+        else:
+            self.user_template = DEFAULT_USER_TEMPLATE_WITHOUT_TOOL
+            user_input = self.user_template.replace('<user_input>', task)
+            user_input = user_input.replace('<role_name>',
+                                            self.builder_cfg.name)
+
+        user_input = user_input.replace('<user_input>', task)
+
+        if 'append_files' in kwargs:
+            append_files = kwargs.get('append_files', [])
+            if len(append_files) > 0:
+                file_names = ','.join(
+                    [os.path.basename(path) for path in append_files])
+                user_input = user_input.replace('<file_names>',
+                                                f'[上传文件{file_names}]')
+            else:
+                user_input = user_input.replace('<file_names>', '')
+        else:
+            user_input = user_input.replace('<file_names>', '')
+
+        return user_input
+
+    def init_prompt(self, task, tool_list, knowledge_list, llm_model,
+                    **kwargs):
+        if len(self.history) == 0:
+
+            self.history.append({
+                'role': 'system',
+                'content': 'You are a helpful assistant.'
+            })
+
+            self.prompt_preprocessor = build_raw_prompt(llm_model)
+
+            if len(tool_list) > 0:
+                prompt = f'{self.system_template}\n{self.instruction_template}'
+
+                # get tool description str
+                tool_str = self.get_tool_str(tool_list)
+                prompt = prompt.replace('<tool_list>', tool_str)
+
+                tool_name_str = self.get_tool_name_str(tool_list)
+                prompt = prompt.replace('<tool_name_list>', tool_name_str)
+            else:
+                self.system_template = DEFAULT_SYSTEM_TEMPLATE_WITHOUT_TOOL
+                prompt = f'{self.system_template}\n{self.instruction_template}'
+
+            user_input = self._update_user_prompt_without_knowledge(
+                task, tool_list, **kwargs)
+
+            if len(knowledge_list) > 0:
+                knowledge_str = self.get_knowledge_str(
+                    knowledge_list, file_name=self.knowledge_file_name)
+                # knowledge
+                prompt = knowledge_str + prompt
+                user_input = user_input.replace('<knowledge_note>',
+                                                '，请查看前面的知识库')
+            else:
+                user_input = user_input.replace('<knowledge_note>', '')
+
+            self.system_prompt = copy.deepcopy(prompt)
+
+            # build history
+            if self.add_addition_round:
+                self.history.append({
+                    'role': 'user',
+                    'content': self.system_prompt
+                })
+                self.history.append({
+                    'role': 'assistant',
+                    'content': self.addition_assistant_reply
+                })
+                self.history.append({'role': 'user', 'content': user_input})
+                self.history.append({
+                    'role': 'assistant',
+                    'content': self.assistant_template
+                })
+            else:
+                self.history.append({
+                    'role': 'user',
+                    'content': self.system_prompt + user_input
+                })
+                self.history.append({
+                    'role': 'assistant',
+                    'content': self.assistant_template
+                })
+
+            self.function_calls = self.get_function_list(tool_list)
+        else:
+            user_input = self._update_user_prompt_without_knowledge(
+                task, tool_list, **kwargs)
+            if len(knowledge_list) > 0:
+                user_input = user_input.replace('<knowledge_note>',
+                                                '，请查看前面的知识库')
+            else:
+                user_input = user_input.replace('<knowledge_note>', '')
+
+            self.history.append({'role': 'user', 'content': user_input})
+            self.history.append({
+                'role': 'assistant',
+                'content': self.assistant_template
+            })
+            if len(knowledge_list) > 0:
+                knowledge_str = self.get_knowledge_str(
+                    knowledge_list, file_name=self.knowledge_file_name)
+                self.update_knowledge_str(knowledge_str)
+
+        return self.system_prompt
+
+    def update_knowledge_str(self, knowledge_str):
+        """update knowledge in this prompt
+
+        Args:
+            knowledge_str (str): knowledge str generated by get_knowledge_str
+
+        """
+
+        for i in range(0, len(self.history)):
+            if self.history[i]['role'] == 'user':
+                content: str = self.history[i]['content']
+                start_pos = content.find(f'{KNOWLEDGE_PROMPT}{self.sep}')
+                end_pos = content.rfind('\n\n# 工具\n\n')
+                if start_pos >= 0 and end_pos >= 0:
+                    self.history[i]['content'] = content[
+                        0:start_pos] + knowledge_str + content[end_pos:]
+                else:
+                    continue
+
+    def get_tool_str(self, tool_list):
+        tool_texts = []
+        for tool in tool_list:
+            tool_texts.append(
+                TOOL_DESC.format(
+                    name_for_model=tool.name,
+                    name_for_human=tool.name,
+                    description_for_model=tool.description,
+                    parameters=json.dumps(tool.parameters,
+                                          ensure_ascii=False)))
+            # + ' ' + FORMAT_DESC['json'])
+        tool_str = '\n\n'.join(tool_texts)
+        return tool_str
+
+    def get_tool_name_str(self, tool_list):
+        tool_name = []
+        for tool in tool_list:
+            tool_name.append(tool.name)
+
+        tool_name_str = json.dumps(tool_name, ensure_ascii=False)
+        return tool_name_str
+
+    def _generate(self, llm_result, exec_result: str):
+        """
+        generate next round prompt based on previous llm_result and exec_result and update history
+        """
+        if len(llm_result) != 0:
+            self.history[-1]['content'] += f'{llm_result}'
+        if len(exec_result) != 0:
+            # handle image markdown wrapper
+            image_markdwon_re = re.compile(
+                pattern=r'!\[IMAGEGEN\]\(([\s\S]+)\)')
+            match = image_markdwon_re.search(exec_result)
+            if match is not None:
+                exec_result = match.group(1).rstrip()
+            exec_result = self.exec_template.replace('<exec_result>',
+                                                     str(exec_result))
+            self.history[-1]['content'] += exec_result
+
+        # generate plate prompt here
+        self.prompt = self.prompt_preprocessor(self.history)
+        return self.prompt
+
+
+def parse_role_config(config: dict):
+    prompt = '你扮演AI-Agent，'
+
+    # concat prompt
+    if 'name' in config and config['name']:
+        prompt += ('你的名字是' + config['name'] + '。')
+    if 'description' in config and config['description']:
+        prompt += config['description']
+    prompt += '\n你具有下列具体功能：'
+    if 'instruction' in config and config['instruction']:
+        if isinstance(config['instruction'], list):
+            for ins in config['instruction']:
+                prompt += ins
+                prompt += '；'
+        elif isinstance(config['instruction'], str):
+            prompt += config['instruction']
+        if prompt[-1] == '；':
+            prompt = prompt[:-1]
+    prompt += '\n下面你将开始扮演'
+    if 'name' in config and config['name']:
+        prompt += config['name']
+    prompt += '，明白了请说“好的。”，不要说其他的。'
+    return prompt
---
+++ modelscope_agent/tools/qrcode_beautify_tool.py
@@ -0,0 +1,246 @@
+import os
+import time
+import json
+import pandas as pd
+import requests
+from io import BytesIO
+from modelscope_agent.tools.tool import Tool, ToolSchema
+from pydantic import ValidationError
+from requests.exceptions import RequestException, Timeout
+
+from modelscope import snapshot_download
+import torch
+from PIL import Image
+import io
+import qrcode
+from requests import get
+from compel import Compel
+import numpy as np
+
+import requests
+from pathlib import Path
+
+from diffusers import (
+    StableDiffusionControlNetPipeline,
+    StableDiffusionPipeline,
+    ControlNetModel,
+    DDIMScheduler,
+    DPMSolverMultistepScheduler,
+    DEISMultistepScheduler,
+    HeunDiscreteScheduler,
+    EulerDiscreteScheduler,
+    EulerAncestralDiscreteScheduler,
+    AutoencoderKL,
+   
+)
+
+Controlnet_MAP={
+    "brightness":"/home/wsco/wyj2/sd模型库/control_v1p_sd15_brightness",
+    "hide":"wyj123456/control_v1p_sd15_qrcode_monster",
+    "nohide":"wyj123456/controlnet_qrcode_sd15_not_hide_code_point"
+}
+
+Model_path_MAP={
+    "GhostMix": "wyj123456/GhostMix",
+    "dreamlike-diffusion-1.0": "/home/wsco/wyj2/sd模型库/dreamlike-diffusion-1.0",
+    "counterfeit": "/home/wsco/wyj2/sd模型库/counterfeit",
+    "SCMix": "/home/wsco/wyj2/sd模型库/SCMix",
+    "Realistic":"/home/wsco/nwr/qrcode_controlnet_realistic/model/realistic"
+
+}
+
+Lora_MAP={
+    "Detail Tweaker":"/home/wsco/nwr/qrcode_controlnet_realistic/lora/add_detail.safetensors",
+    "Detail Slider":"/home/wsco/nwr/qrcode_controlnet_realistic/lora/detail_slider_v4.safetensors",
+    "None":"None"
+}
+
+SAMPLER_MAP = {
+    "DPM++ Karras SDE": lambda config: DPMSolverMultistepScheduler.from_config(config, use_karras=True, algorithm_type="sde-dpmsolver++"),
+    "DPM++ Karras": lambda config: DPMSolverMultistepScheduler.from_config(config, use_karras=True),
+    "Heun": lambda config: HeunDiscreteScheduler.from_config(config),
+    "Euler a": lambda config: EulerAncestralDiscreteScheduler.from_config(config),
+    "Euler": lambda config: EulerDiscreteScheduler.from_config(config),
+    "DDIM": lambda config: DDIMScheduler.from_config(config),
+    "DEIS": lambda config: DEISMultistepScheduler.from_config(config),
+}
+
+os.environ.get('QRCODEAPI_URL', '')
+
+class QrcodeBeautify(Tool):    
+    description = 'QRcode是一个美化二维码的工具，输入用户上传二维码并输入要求，生成美化后的二维码。'
+    name = 'QRcode_beautify'
+    parameters: list = [{
+        'name': 'prompt',
+        'description': '用户输入提示词',
+        'required': True,
+    },
+    {
+        'name': 'image',
+        'description': '用户上传二维码图片路径',
+        'required': True,
+    }
+    ]
+
+    def _local_call(self, *args, **kwargs):
+        prompt = kwargs['prompt']
+        image = kwargs['image']
+        print(image)
+        file_path = self.state['file_path']
+        file = self.state['file']
+        return {'result': f'已完成ECS实例ID为{prompt}的续费，续费时长{file_path}月'}
+    
+    def _remote_call(self, *args, **kwargs):
+        prompt = kwargs['pompt']
+        image = kwargs['image']
+        print(image)
+        file_path = self.state['file_path']
+        file = self.state['file']
+        return {'result': f'已完成ECS实例ID为{prompt}的续费，续费时长{file_path}月'}
+
+    def __init__(self, cfg={}):
+        self.cfg = cfg.get(self.name, {})
+        try:
+            all_param = {
+                'name': self.name,
+                'description': self.description,
+                'parameters': self.parameters
+            }
+            self.tool_schema = ToolSchema(**all_param)
+        except ValidationError:
+            raise ValueError(f'Error when parsing parameters of {self.name}')
+
+        # self._str = self.tool_schema.model_dump_json()
+        self._function = self.parse_pydantic_model_to_openai_function(
+            all_param)
+
+
+
+
+    def __call__(self, *args, **kwargs):
+
+        prompt = kwargs['prompt']
+        # image_path = kwargs['image']
+        image_path = self.state['file_path']
+        if image_path is None:
+            return "请上传二维码图片"
+        data = {
+            "prompt": prompt
+        }
+
+        local_file = Path(image_path)
+        content = local_file.read_bytes()
+        response = requests.post(url, files={'file': BytesIO(content)}, data=data)
+
+        # 检查响应
+        if response.status_code == 200:
+        
+            # 获取文件名
+            filename = response.headers["Content-Disposition"].split("filename=")[1].strip('\"')
+            # 保存文件
+            # with open("/home/wsco/zcg/project/qrcode_beautify_2.0/facechain_agent/temp/"+filename, "wb") as file:
+            #     file.write(response.content)    
+            print(f"File '{filename}' received successfully.")
+            delete_path = "/home/wsco/zcg/project/qrcode/temp/delete.png"
+            delete_path = delete_path.replace('.', str(np.random.randint(1,999)) + '.')
+            Image.open(BytesIO(response.content)).save(delete_path)
+            # return {'result' : f'![]({delete_path})'}
+            return {'result' : f'{delete_path}'}
+        else:
+            # 请求失败
+            print("Error:", response.status_code, response.text)
+
+        
+        # data = {
+        #     "prompt" : prompt,
+        #     "image_path": image_path
+        # }
+        # response = requests.post(url,json=data)
+        # if response.status_code == 200:
+        #     # 将图像数据读取为 PIL 图像对象
+        #     result = Image.open(BytesIO(response.content))
+
+        #     return result
+        # else:
+        #     print(f"Request failed with status code: {response.status_code}\n{response}")
+        
+        # qrcode_image = Image.open(image_path)
+        # negative_prompt = "ugly, disfigured, low quality, blurry, nsfw"
+
+        # controlnet_path="nohide"
+        # lora_path="Detail Tweaker"
+        # model="GhostMix"
+        # sampler="Euler a"
+
+        # guidance_scale: float = 10.0
+        # controlnet_conditioning_scale: float = 2.0
+        # num_inference_steps=40
+        # lora_w=0.8
+        
+        # # resize_for_condition_image
+        # input_image = qrcode_image.convert("RGB")
+        # W, H = input_image.size
+        # k = float(768) / min(H, W)
+        # H *= k
+        # W *= k
+        # H = int(round(H / 64.0)) * 64
+        # W = int(round(W / 64.0)) * 64
+        # qrcode_image = input_image.resize((W, H), resample=Image.LANCZOS)
+
+        # model_dir_con = snapshot_download(Controlnet_MAP[controlnet_path],revision='v1.0.0')
+        # controlnet = ControlNetModel.from_pretrained(
+        # model_dir_con,
+        # torch_dtype=torch.float32
+        # ).to('cpu')
+        # base_model = snapshot_download(Model_path_MAP[model],revision='v1.0.0')
+        # pipe = StableDiffusionControlNetPipeline.from_pretrained(
+        #     base_model,
+        #     controlnet=controlnet,
+        #     #vae=vae,
+        #     # safety_checker=None,
+        #     torch_dtype=torch.float32
+        # ).to('cuda')
+        # if lora_path != 'None':
+        #     # print("Use lora!")
+        #     ### add lora
+        #     pipe.unload_lora_weights()
+        #     pipe.load_lora_weights(Lora_MAP[lora_path])
+        #     #lora_w = 0.8
+        #     pipe._lora_scale = lora_w
+        #     state_dict, network_alphas = pipe.lora_state_dict(
+        #         Lora_MAP[lora_path]
+        #     )
+
+        #     #network_alpha = network_alpha * lora_w
+        #     pipe.load_lora_into_unet(
+        #         state_dict = state_dict
+        #         , network_alphas = network_alphas
+        #         , unet = pipe.unet
+        #     )
+
+        #     pipe.load_lora_into_text_encoder(
+        #         state_dict = state_dict
+        #         , network_alphas = network_alphas
+        #         , text_encoder = pipe.text_encoder
+        #     )
+        #     compel_proc = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder)
+        # pipe.scheduler = SAMPLER_MAP[sampler](pipe.scheduler.config)
+        # pipe.enable_xformers_memory_efficient_attention()
+
+        # out = pipe(
+        #     #prompt_embeds=
+        #     # prompt=prompt,
+        #     prompt_embeds=compel_proc(prompt),
+        #     #negative_prompt_embeds=
+        #     # negative_prompt=negative_prompt,
+        #     negative_prompt_embeds=compel_proc(negative_prompt),
+            
+        #     image=qrcode_image,
+        #     width=768,
+        #     height=768,
+        #     guidance_scale=float(guidance_scale),
+        #     controlnet_conditioning_scale=float(controlnet_conditioning_scale),
+        #     num_inference_steps=int(num_inference_steps),
+            
+        # )
+        # return out.images[0]
